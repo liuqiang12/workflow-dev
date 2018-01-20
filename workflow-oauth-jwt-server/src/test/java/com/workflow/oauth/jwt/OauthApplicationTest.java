3fd56012efcf783c26b647964ff5e334a8d9b886
刘强 <784248173@qq.com>
2018-01-20 14:41:11 +0800

    OAUTH2.0   ---   20%

--- workflow-oauth-jwt-server/src/test/java/com/workflow/oauth/jwt/OauthApplicationTest.java
+++ workflow-oauth-jwt-server/src/test/java/com/workflow/oauth/jwt/OauthApplicationTest.java
@@ -1,37 +1,8 @@
-/*
 package com.workflow.oauth.jwt;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.test.context.junit4.SpringRunner;
-
-
-@RunWith(SpringRunner.class)
-@EnableCaching//缓存支持
-@SpringBootTest
+/**
+ * Created by DELL on 2018/1/20.
+ */
 public class OauthApplicationTest {
 
-    */
-/*@Autowired
-    private DataSource dataSource;*//*
-
-
-    */
-/*@Autowired
-    private com.workflow.common.dao.UserDao UserDao;*//*
-
-
-    @Test
-    public void TestDataSource(){
-//        long count = notificationDao.getNotificationCount(72);
-//        System.out.println(count);
-//        List<Notification> list = notificationDao.getByTouser(UserDao.findOne(2));
-//        System.out.println(list);
-//        list.forEach(t->{
-//            System.out.println(t.getPosts().getTitle());
-//        });
-    }
 }
-*/
