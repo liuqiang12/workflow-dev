cdb5027375479f1f9e126bb914db904b53bd51cf
刘强 <784248173@qq.com>
2018-01-20 15:33:01 +0800

    OAUTH2.0   ---   20%

--- workflow-oauth-jwt-server/src/main/java/com/workflow/oauth/jwt/config/ComponentScanConfig.java
+++ workflow-oauth-jwt-server/src/main/java/com/workflow/oauth/jwt/config/ComponentScanConfig.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Configuration;
  * 扫描路径
  */
 @Configuration
-@ComponentScan(basePackages = "com.workflow.common")
-@ComponentScan(basePackages = "com.workflow.oauth")
+@ComponentScan(basePackages = {"com.workflow.common","com.workflow.oauth"})
 public class ComponentScanConfig {
 
 }
