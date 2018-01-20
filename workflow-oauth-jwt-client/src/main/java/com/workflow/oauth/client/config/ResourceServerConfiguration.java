e549c1e13a4fc49ff9668518960d3dfa096f51e1
刘强 <784248173@qq.com>
2018-01-20 13:03:15 +0800

    OAUTH2.0   ---   20%

--- workflow-oauth-jwt-client/src/main/java/com/workflow/oauth/client/config/ResourceServerConfiguration.java
+++ workflow-oauth-jwt-client/src/main/java/com/workflow/oauth/client/config/ResourceServerConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
