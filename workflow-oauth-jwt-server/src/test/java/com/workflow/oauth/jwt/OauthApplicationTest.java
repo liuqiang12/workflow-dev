87297ca100bbf01c63dbbe88c9e176094da714c8
刘强 <784248173@qq.com>
2018-01-20 14:33:58 +0800

    OAUTH2.0   ---   20%

---
+++ workflow-oauth-jwt-server/src/test/java/com/workflow/oauth/jwt/OauthApplicationTest.java
@@ -0,0 +1,37 @@
+/*
+package com.workflow.oauth.jwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@EnableCaching//缓存支持
+@SpringBootTest
+public class OauthApplicationTest {
+
+    */
+/*@Autowired
+    private DataSource dataSource;*//*
+
+
+    */
+/*@Autowired
+    private com.workflow.common.dao.UserDao UserDao;*//*
+
+
+    @Test
+    public void TestDataSource(){
+//        long count = notificationDao.getNotificationCount(72);
+//        System.out.println(count);
+//        List<Notification> list = notificationDao.getByTouser(UserDao.findOne(2));
+//        System.out.println(list);
+//        list.forEach(t->{
+//            System.out.println(t.getPosts().getTitle());
+//        });
+    }
+}
+*/
